e9893aceb0ccd76a72a9c11c5c41ec1d018e7752
GustaHideki <guyokoyama@hotmail.com>
2023-03-29 14:47:08 -0300

    upload depastas

---
+++ JavaScript/3- String.js
@@ -0,0 +1,16 @@
+
+
+
+
+let myFirstString = "E aqui dentro eu posso escrever o que eu quiser!"
+let mySeccondString = 'agora com aspas simples'
+let mythirdString =  `Com a crase eu também consigo`
+
+let text = "fulano de tal falou o seguinte: 'Eu aprendi JS' "
+
+let numberOfPeopleInClass = 34
+
+let myTextWithSuperPowers = `tinham exatamente ${10 + 20 * 4} pessoas na aula`
+
+
+console.log(myTextWithSuperPowers)
