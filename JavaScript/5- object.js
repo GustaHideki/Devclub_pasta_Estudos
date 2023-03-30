6fc810439f3337f724f43e67075db7d58c4ca6ea
GustaHideki <guyokoyama@hotmail.com>
2023-03-30 10:48:51 -0300

    novas aulas

---
+++ JavaScript/5- object.js
@@ -0,0 +1,14 @@
+
+const person ={
+name: "Gustavo Hideki",
+age: 19,
+sex: "male",
+height: 1.7,
+address: {
+street: "Numidia",
+number:280,
+country: "Brazil"
+}
+
+}
+console.log(person.address)
