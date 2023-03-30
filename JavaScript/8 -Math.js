b76adc1285e67cfd84ffff2996485a90fd80f37a
GustaHideki <guyokoyama@hotmail.com>
2023-03-30 14:44:08 -0300

    aula sobre math

---
+++ JavaScript/8 -Math.js
@@ -0,0 +1,7 @@
+/* MATH*/
+
+console.log(Math.PI) //Mostra o valor de PI
+console.log(Math.pow(2,3)) // Permite usar potenciação
+console.log(Math.sqrt(25,2)) // permite calcular raiz de um numero
+console.log(Math.ceil(26.3)) //Arrendonda um numero para "CIMA"
+console.log(Math.floor(26.3)) // arredonda um numero para "BAIXO"
