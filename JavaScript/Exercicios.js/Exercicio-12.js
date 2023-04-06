fde29efe9f8ca07312b5dfb4ca295232484a171e
GustaHideki <guyokoyama@hotmail.com>
2023-04-06 10:15:30 -0300

    novos exercicios em js

---
+++ JavaScript/Exercicios.js/Exercicio-12.js
@@ -0,0 +1,7 @@
+/*Faça um programa onde colocamos dois numeros ímpares e o programa imprima o resto da divisão. Ex: 7/3 deve imprimir: 1, 25/5 deve imprimir: 0.*/
+
+const numero1 = 7
+const numero2 = 3
+const resto = numero1 % numero2
+
+console.log(resto)
---
+++ JavaScript/Exercicios.js/Exercicio-9.js
@@ -0,0 +1,43 @@
+// Criei 5 Objetos, neles devem conter os dados de 5 pessoas que você conhece. Mínimo 5 dados por pessoa.
+
+const person1= {
+    nome: "Gustavo Hideki",
+    idade: 19,
+    sexo: "masculino",
+    email: "guyokoyama@hotmail.com",
+    celular:11920024461    
+}
+
+const person2= {
+    nome: "Gabriel Takeshi",
+    idade: 17,
+    sexo: "masculino",
+    email: "takeshigames123@hotmail.com",
+    celular:11950875142    
+}
+
+const person3= {
+    nome: "Marcio Yokoyama",
+    idade: 49,
+    sexo: "masculino",
+    email: "yokoyamamarcio@hgmail.com",
+    celular:11973271554   
+}
+
+const person4= {
+    nome: "Willian eidi",
+    idade: 20,
+    sexo: "masculino",
+    email: "willianeidi@hotmail.com",
+    celular:43920010238    
+}
+
+const person5= {
+    nome: "Gustavo Erthal",
+    idade: 20,
+    sexo: "masculino",
+    email: "erthal_gustavo@hotmail.com",
+    celular:4396081091 
+}
+
+
