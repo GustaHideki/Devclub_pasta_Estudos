d51dfe0b41c69680f611cd5c8a130acbcac5a025
GustaHideki <guyokoyama@hotmail.com>
2023-04-09 20:06:15 -0300

    operadores typeof e delete

---
+++ JavaScript/operadores-typeof-delete.js
@@ -0,0 +1,17 @@
+/* OPERADORES typeof e delete*/
+
+const myNumber = 20
+const myString = "Olá, sou uma string"
+
+const myObject = {
+    name: "João",
+    age : 20,
+    height: 1.7
+}
+
+delete myObject.age
+
+console.log(myObject)
+console.log(typeof myNumber)
+console.log(typeof myString)
+
