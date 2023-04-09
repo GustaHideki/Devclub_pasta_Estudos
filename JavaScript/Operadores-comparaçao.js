d243cdbb0d7ac9a6366505103fd78ab43fce705d
GustaHideki <guyokoyama@hotmail.com>
2023-04-09 16:39:01 -0300

    novas aulas em java

---
+++ JavaScript/Operadores-comparaçao.js
@@ -0,0 +1,10 @@
+/*
+ == -> Igual // Ele compara o valor, mas não compara o tipo do valor.
+
+ === -> TOTALMENTE IGUAL //Ele verifica o tipo do dado.
+
+ != -> Diferente // Ele compara o valor, mas não compara o tipo do valor.
+
+ !== -> TOTALMENTE DIFERENTE // Ele verifica o tipo do dado.
+
+*/
