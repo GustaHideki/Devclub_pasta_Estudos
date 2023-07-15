a65322dd80a21559701182c4e2d370fa9182a9ae
GustaHideki <guyokoyama@hotmail.com>
2023-07-15 18:46:11 -0300

    Math

---
+++ novas aulas javascript/javasrcipt 3/Math.js
@@ -0,0 +1,30 @@
+/*
+Math -> matemática
+
+- pow(2,2) /potência / 2² = 4
+- sqrt(25,2) / Raíz / √25
+-PI / 3,14
+-Ceil / teto -> arredondar para cima
+-floor / chão -> arredondar para baixo
+
+-ramdon() / número aleatório entre 0 e 1
+*/
+
+
+const result = Math.pow(2,2)
+console.log(result)
+
+const result1 = Math.sqrt(25,2)
+console.log(result1)
+
+const result3 = Math.PI
+console.log(result3)
+
+const result4 = Math.ceil(2.2)
+console.log(result4)
+
+const result5 = Math.floor(2.2)
+console.log(result5)
+
+const result6 = Math.random()
+console.log(result6)
